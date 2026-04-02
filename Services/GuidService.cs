e70cd98c909b0dfae04a38aee43763c90c042370
Richu Mathews <richu.ms@pitsolutions.com>
2026-04-02 09:24:45 +0530

    Initial commit - .NET project

---
+++ Services/GuidService.cs
@@ -0,0 +1,45 @@
+﻿namespace CustomWebAPI.Services
+{
+    // Interfaces
+    public interface ISingletonService { Guid GetGuid(); }
+    public interface IScopedService { Guid GetGuid(); }
+    public interface ITransientService { Guid GetGuid(); }
+    public class SingletonService : ISingletonService
+    {
+        private readonly Guid _guid;
+        public SingletonService()
+        {
+            _guid = Guid.NewGuid();
+        }
+        public Guid GetGuid()
+        {
+            return _guid;
+        }
+    }
+
+    public class ScopedService : IScopedService
+    {
+        private readonly Guid _guid;
+        public ScopedService()
+        {
+            _guid = Guid.NewGuid();
+        }
+        public Guid GetGuid()
+        {
+            return _guid;
+        }
+    }
+
+    public class TransientService : ITransientService
+    {
+        private readonly Guid _guid;
+        public TransientService()
+        {
+            _guid = Guid.NewGuid();
+        }
+        public Guid GetGuid()
+        {
+            return _guid;
+        }
+    }
+}
