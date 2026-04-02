b5788d311f830115d684208cb219592ba44e92be
Richu Mathews <richu.ms@pitsolutions.com>
2026-04-02 09:43:26 +0530

    Remove all code from main

--- Controllers/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomWebAPI.Controllers.Models
-{
-    public class LoginModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
